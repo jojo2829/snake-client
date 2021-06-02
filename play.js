a8f78aabc7a07f08083fa25453cc1f3f6101bcfb
jojo2829 <joulinli@gmail.com>
2021-06-02 21:47:50 +0000

    added exit key command

--- play.js
+++ play.js
@@ -3,5 +3,31 @@ const connect = require("./client");
 
 connect();
 
+// setup interface to handle user input from stdin
 
+const handleUserInput = function () {
+  
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("exit game");
+      process.exit();
+    }
+  });
+  
+};
+
+handleUserInput();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
 
